4a36ae8a6a8e41a631bb2737f89ec4dd46b31850
Fotihjon <ikromov@usf.edu>
2025-02-02 22:17:20 -0500

    Task 2 - Sales Report.

--- cc_4.js
+++ cc_4.js
@@ -5,3 +5,12 @@ if (purchaseAmount > 100) {
     finalAmount = purchaseAmount * 0.9
 }
 console.log(finalAmount)
+
+//Task 2 - Sales Report.
+let sales = [120, 85, 200, 150, 90]
+let totalSales = 0;
+for (let index = 0; index < sales.length; index++) {
+    const sale = sales[index];
+    totalSales += sale
+}
+console.log(totalSales)
